18cff21cf3eb2c7871efa0ef89263e371905a11b
KAMLESH LOVEWANSHI <kamleshlovewanshi2000@gmail.com>
2022-07-14 21:07:24 +0530

    Initial commit

--- src/CloningOfArrays.java
+++ src/CloningOfArrays.java
@@ -1,10 +1,12 @@
-/*When you clone a single-dimensional array, such as Object[],
- a “deep copy” is performed with the new array containing copies
-  of the original array’s elements as opposed to references.
- */
-
 public class CloningOfArrays {
     public static void main(String[] args) {
+        /*  Deep Copy:
+            When you clone a single-dimensional array, such as Object[],
+            a “deep copy” is performed with the new array containing copies
+            of the original array’s elements as opposed to references .
+        */
+
+       /*
         int arr[] = {1,2,3,4,5,};
         int cloneArray[] = arr.clone();
         // will print false as deep copy is created
@@ -15,5 +17,25 @@ public class CloningOfArrays {
         {
             System.out.print(arr[i]+" ");
         }
+        */
+
+        /*  Shallow Copy:
+        A clone of a multi-dimensional array (like Object[][])
+        is a “shallow copy,” however, which is to say that it
+        creates only a single new array with each element array
+        a reference to an original element array, but subarrays are shared.
+        */
+
+        int intArray[][] = { { 1, 2, 3 }, { 4, 5 } };
+
+        int cloneArray[][] = intArray.clone();
+
+        // will print false
+        System.out.println(intArray == cloneArray);
+
+        // will print true as shallow copy is created
+        // i.e. sub-arrays are shared
+        System.out.println(intArray[0] == cloneArray[0]);
+        System.out.println(intArray[1] == cloneArray[1]);
     }
 }
