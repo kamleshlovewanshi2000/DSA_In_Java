8b080c2aef7d3e5b4d7de6061072baf838583179
KAMLESH LOVEWANSHI <kamleshlovewanshi2000@gmail.com>
2022-07-14 15:48:31 +0530

    Initial commit

---
+++ src/Create_Array.java
@@ -0,0 +1,15 @@
+public class Create_Array {
+    public static void main(String[] args) {
+        //int arr[];      // Declaring array
+        //arr = new int[10];      // allocating memory to array
+        //int[] intArray = new int[20]; // Declaring and allocating
+                                      // memory to array both are done in one line
+        int[] arr = new int[]{1,2,3,4,5,6,7,8,9,10}; // Array Literal
+
+        // Accessing Array element using for loop.
+        for(int i=0;i<arr.length; i++)
+        {
+            System.out.println("Elements at index "+i+":"+arr[i]);
+        }
+    }
+}
