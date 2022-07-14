817f59f238c38616d10b0ddf7e1a4819ab577877
KAMLESH LOVEWANSHI <kamleshlovewanshi2000@gmail.com>
2022-07-14 20:44:09 +0530

    Initial commit

--- src/CloningOfArrays.java
+++ src/CloningOfArrays.java
@@ -1,3 +1,8 @@
+/*When you clone a single-dimensional array, such as Object[],
+ a “deep copy” is performed with the new array containing copies
+  of the original array’s elements as opposed to references.
+ */
+
 public class CloningOfArrays {
     public static void main(String[] args) {
         int arr[] = {1,2,3,4,5,};
