0aa464a086e30836ef7580e3b520ecb12856291a
KAMLESH LOVEWANSHI <kamleshlovewanshi2000@gmail.com>
2022-07-17 23:31:58 +0530

    Initial commit

--- src/Array/Array Rotations/Array_rotation.java
+++ src/Array/Array Rotations/Array_rotation.java
@@ -1,4 +1,3 @@
-import java.io.*;
 public class Array_rotation {
         /*
          int arr[] = {1,2,3,4,5,6,7};
@@ -78,4 +77,46 @@ public class Array_rotation {
              rotate.lefRotate(arr, 15,13);
              rotate.printArray(arr,13);
         }
+
+    public static class CloningOfArrays {
+        public static void main(String[] args) {
+            /*  Deep Copy:
+                When you clone a single-dimensional array, such as Object[],
+                a “deep copy” is performed with the new array containing copies
+                of the original array’s elements as opposed to references .
+            */
+
+           /*
+            int arr[] = {1,2,3,4,5,};
+            int cloneArray[] = arr.clone();
+            // will print false as deep copy is created
+            // for one-dimensional array.
+            System.out.println(arr==cloneArray);
+            // Printing clone array elements
+            for(int i=0; i<cloneArray.length;i++)
+            {
+                System.out.print(arr[i]+" ");
+            }
+            */
+
+            /*  Shallow Copy:
+            A clone of a multi-dimensional array (like Object[][])
+            is a “shallow copy,” however, which is to say that it
+            creates only a single new array with each element array
+            a reference to an original element array, but subarrays are shared.
+            */
+
+            int intArray[][] = { { 1, 2, 3 }, { 4, 5 } };
+
+            int cloneArray[][] = intArray.clone();
+
+            // will print false
+            System.out.println(intArray == cloneArray);
+
+            // will print true as shallow copy is created
+            // i.e. sub-arrays are shared
+            System.out.println(intArray[0] == cloneArray[0]);
+            System.out.println(intArray[1] == cloneArray[1]);
+        }
+    }
 }
---
+++ src/Array/Array Rotations/ReversalAlgoForArrRotation.java
@@ -0,0 +1,43 @@
+public class ReversalAlgoForArrRotation {
+    // Reversal Algorithm of Array Rotation
+    static  void lefRotate(int arr[], int d){
+        if (d==0)
+            return;
+
+        int n = arr.length;
+        // In case the rotating factor is
+        // greater than array length.
+        d = d%n;
+        reverseArray(arr, 0, d-1);
+        reverseArray(arr, d, n-1);
+        reverseArray(arr, 0, n-1);
+    }
+    /* Fuction to reverse arr[] from index start to end */
+    static void reverseArray(int arr[], int start, int end)
+    {
+        int temp;
+        while(start<end)
+        {
+            temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+    //Utility Function
+    // Function to print an array
+    static  void printArray(int arr[])
+    {
+        for(int i=0; i<arr.length;i++)
+            System.out.print(arr[i]+" ");
+    }
+    // Drive Code to test above functions
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4,5,6,7};
+        int n = arr.length;
+        int d = 9;
+        lefRotate(arr,d);
+        printArray(arr);
+    }
+}
